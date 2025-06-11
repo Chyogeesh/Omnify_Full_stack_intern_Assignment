a4e6a91f9d800f9586146844a6a2a04ca8c6f710
Ch Yogeesh Sai <108213999+Chyogeesh@users.noreply.github.com>
2025-06-11 12:03:52 +0530

    Create api.js

---
+++ api.js
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/lib/prisma";
+import bcrypt from "bcrypt";
+
+export default NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user) throw new Error("User not found");
+
+        const isValid = await bcrypt.compare(credentials.password, user.password);
+
+        if (!isValid) throw new Error("Invalid password");
+
+        return { id: user.id, email: user.email };
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/login'
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
